cc340a26cda9db2188bec08a077eb018c4465c87
mxx <mengxxself>
2017-04-06 17:26:18 +0800

    koa

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-var koa = require('koa');
-var app = koa();
+let koa = require('koa');
+let app = new koa();
+let Router = require('koa-router')
+let router = new Router()
+let render = require('koa-ejs')
+let path = require('path');
+render(app, {
+    root: path.join(__dirname, 'views'),
+    layout: 'index',
+    viewExt: 'html',
+    cache: false,
+    debug: true
+});
 
-app.use(function *(){
+router.get('/user/:id', function * (next) {
+    console.log(this.params);
+    this.body = 'say moring'
+    yield next
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.use(function * (next) {
   this.body = 'Hello World';
+  yield this.render('index',{koaRender:'try layout'})
+  yield next
+
 });
 
 app.listen(3000);
